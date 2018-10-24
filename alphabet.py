fec213a7f4a0af9e2addf2e682b7fc4d54508ad0
gokulrock542 <43330704+gokulrock542@users.noreply.github.com>
2018-10-24 06:37:33 -0700

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("alphabet");
+    else
+        printf("no");
+
+    return 0;
+    getch();
+}
