4a3c1a791715a252357b489d0db356a06b3529fd
gokulrock542 <43330704+gokulrock542@users.noreply.github.com>
2019-02-21 14:10:38 +0530

    Update amstrong.c

--- amstrong.c
+++ amstrong.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include<conio.h>
 void main()
 {
     int num,o,r,result = 0;
     printf("input:");
     scanf("%d", &num);
     o = num;
-    while (o!= 0)
-    {
-        r = o%10;
-        result += r*r*r;
-        o= o/10;
-    }
-    printf("output:");
-    if(result == num)
-        printf("yes");
-    else
-        printf("no");
+while (o!= 0)
+{
+  r = o%10;
+  result += r*r*r;
+  o= o/10;
+}
+ printf("output:");
+ if(result == num)
+     printf("yes");
+ else
+     printf("no");
 }
